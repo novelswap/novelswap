8fc334dbcb1d644c172a60e16b9c1fe409d501d8
Josh Ghent <me@joshghent.com>
2017-02-18 18:53:52 +0000

    Added homepage. Added header and footer.

---
+++ components/header.inc.php
@@ -0,0 +1,12 @@
+<?php
+if (empty($pageTitle)) {
+  $pageTitle = 'NovelSwap';
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title><?php echo $pageTitle ?></title>
+</head>
---
+++ public_html/index.php
@@ -0,0 +1,12 @@
+<?php
+$pageTitle = 'NovelSwap | Home';
+?>
+
+<?php include('./../components/header.inc.php'); ?>
+<div>
+
+</div>
+
+<?php include('./../components/footer.inc.php'); ?>
+</body>
+</html>
